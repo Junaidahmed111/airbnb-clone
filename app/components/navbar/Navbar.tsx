2f259be1890f03acd352199251a9f5a6bd02f055
Junaidahmed111 <ahmedjuniad30@gmail.com>
2024-01-18 23:23:07 +0500

    navbar added in layout

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>The Navbar</div>;
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
-
+import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 
 const font = Nunito({
