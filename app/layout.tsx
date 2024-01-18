c8ee6b702a7f75b926db01d8aca1204197307415
Junaidahmed111 <ahmedjuniad30@gmail.com>
2024-01-18 23:04:11 +0500

    Nunito fonts added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Nunito({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
