c1748128ef06db60e571f8f717ca3ea458e808b1
Junaidahmed111 <ahmedjuniad30@gmail.com>
2024-01-18 23:40:36 +0500

    container component created

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import Container from "../Container";
 const Navbar = () => {
   return (
     <div className="fixed z-10 w-full bg-green-200 shadow-sm">
-      <div className="py-4 border-b-[1px]"></div>
+      <div className="py-4 border-b-[1px]">
+        <Container></Container>
+      </div>
     </div>
   );
 };
