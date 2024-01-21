414970fd762d01b83bd13a55c57a7733d0783f55
Junaidahmed111 <ahmedjuniad30@gmail.com>
2024-01-21 14:39:15 +0500

    login modal styling

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -82,6 +82,7 @@ const Modal: React.FC<ModalProps> = ({
                 >
                   <IoMdClose size={18} />
                 </button>
+                <div className="text-lg font-semibold">{title}</div>
               </div>
             </div>
           </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <Navbar />
-        <Modal isOpen />
+        <Modal isOpen title="login modal" />
         {children}
       </body>
     </html>
